576523517ffc3e00098618b77f7589867cc97348
Parastud <parthsharma07070@gmail.com>
2026-04-09 00:49:59 +0530

    end point update

--- app.env.ts
+++ app.env.ts
@@ -1,7 +1,7 @@
 export const BASEURL = 'http://api.ogs.work.gd';
 // export const BASEURL = 'http://the-ogs.ap-south-1.elasticbeanstalk.com';
 
-export const IS_DEV = false
+export const IS_DEV = true
 
 export const PRIVACY_POLICY_URL = "https://hackvision-server-latest.onrender.com"
 export const TERMS_AND_CONDITIONS_URL = "https://hackvision-server-latest.onrender.com"
